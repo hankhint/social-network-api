147cdb47e6bc1e145b3194a769c566efef2a93f3
HankHint <hankhint@gmail.com>
2022-03-07 15:25:04 -0600

    added server files from module

---
+++ server.js
@@ -0,0 +1,25 @@
+
+//importing modules
+const express = require('express');
+const mongoose = require('mongoose');
+
+//setting variables to be used for running the app and connecting the app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+//lets express access the routes
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
